f81dbc4c5022f599598320c32e3dd968eabd90b4
Krish-Bagaria <Krishbagaria@gmail.com>
2025-09-19 00:51:53 +0530

    Weather App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Weather from "./component/Weather";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Weather />
+      </div>
+    </>
+  );
+}
+
+export default App;
